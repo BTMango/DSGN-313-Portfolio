2a95b7acd861baf2054175213a031a613ad7b313
Denis Rosales Becerra <denis2005jr@gmail.com>
2025-01-13 18:23:18 -0700

    Updated Portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -1,13 +1,13 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "John Doe",
+    name: "Denis Rosales Becerra",
     bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
     contact: {
-        email: "john.doe@gmail.com",
-        phone: "+12345678910",
-        linkedin: "https://www.linkedin.com/",
-        github: "https://www.github.com/",
+        email: "denis2005jr@gmail.com",
+        phone: "+5876643370",
+        linkedin: "https://www.linkedin.com/in/DenisRosalesBecerra/",
+        github: "https://www.github.com/BTMango",
     }
 }
 
